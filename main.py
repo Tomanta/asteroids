7301ad1700127c65e2dfb04bc111ec0b167d2789
Tomanta <bzednick@gmail.com>
2024-12-08 20:19:59 -0600

    Added fps clock

--- main.py
+++ main.py
@@ -4,21 +4,21 @@
 import pygame
 from constants import *
 
-def game_loop(screen):
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
-        screen.fill(pygame.Color(0,0,0))
+        screen.fill("black")
         pygame.display.flip()
-
-
-def main():
-    pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    game_loop(screen)
-
+        dt = clock.tick(60) / 1000 # convert ms to seconds
 
 if __name__ == "__main__":
     main()
