d7b20d83a383d51a6574327c1943f07b28ef9df8
Tomanta <bzednick@gmail.com>
2024-12-09 20:59:18 -0600

    Added shot timer

--- player.py
+++ player.py
@@ -7,6 +7,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shot_timer = 0
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -24,6 +25,9 @@ class Player(CircleShape):
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        self.shot_timer -= dt
+        if self.shot_timer < 0:
+            self.shot_timer = 0
 
         if keys[pygame.K_a]:
             self.rotate(-dt)
@@ -41,6 +45,10 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt
 
     def shoot(self):
+        if self.shot_timer > 0:
+            return # don't allows shots if over rate limit
+        
         shot_velocity = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
-        shot = Shot(self.position.x, self.position.y)
+        shot = Shot(self.position[0], self.position.y)
         shot.set_velocity(shot_velocity)
+        self.shot_timer = PLAYER_SHOT_COOLDOWN
